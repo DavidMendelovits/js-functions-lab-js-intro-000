ed7b16b48fc2fddcf3809c35b77536f95ac44011
David Mendelovits <david.mendelovits@gmail.com>
2017-12-13 19:41:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() {
 return ("Happy holidays!")
 }
 
-function happyHolidaysTo(you) {
-return ('Happy holidays, ${you}!')
+function happyHolidaysTo(name) {
+return (`Happy holidays, ${name}!`)
 }
